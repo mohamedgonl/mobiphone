a127231417f4d53f0c629cbece67f5caa693456f
Gonl <long.nq.hust@gmail.com>
2022-09-22 14:01:29 +0700

    update guide

---
+++ src/components/Body/Guide/datatest.js
@@ -0,0 +1,68 @@
+import img1 from "../../../images/img1.jpg";
+import img2 from "../../../images/img2.jpg";
+import img3 from "../../../images/img3.jpg";
+import img4 from "../../../images/img4.jpg";
+import img5 from "../../../images/img5.jpg";
+
+const guideSteps1 = [
+    { i: 1, content: "Android. Trong mục cài đặt (Settings), Chọn kết nối (Connection)", img: img1 },
+    {
+      i: 2,
+      content:
+        "Android. Chọn quản lý Sim (Sim card manager) và chọn thêm gói di động (Add mobile plan)",  img: img2
+    },
+    {
+      i: 3,
+      content: "Android. Chọn Thêm gói cước di động (Add mobile plan)",  img: img3
+    },
+    {
+      i: 4,
+      content: "Android. Chọn mục Thêm bằng mã QR (Add using QR code)",  img: img4
+    },
+    {
+      i: 5,
+      content:
+        "Android. Đưa camera hướng vào mã QR sao cho mã QR nằm trong đường hướng dẫn để quét",  img: img5
+    },
+    {
+      i: 6,
+      content: "Android. Khi gói di động được quét ra, chạm vào Thêm (Add) để đăng ký",  img: img1
+    },
+    {
+      i: 7,
+      content: "Android. Chạm vào OK để bật gói di động",  img: img2
+    },
+    {
+      i: 8,
+      content:
+        "Android. Kết quả: eSIM sau khi được kích hoạt sẽ xuất hiện trong mục eSIM như hình",  img: img3
+    },
+  ];
+
+
+
+  const guideSteps2 = [
+    { i: 1, content: "IOS. Trong mục cài đặt (Settings), Chọn kết nối (Connection)", img: img1 },
+    {
+      i: 2,
+      content:
+        "IOS. Chọn quản lý Sim (Sim card manager) và chọn thêm gói di động (Add mobile plan)",  img: img2
+    },
+    {
+      i: 3,
+      content: "IOS. Chọn Thêm gói cước di động (Add mobile plan)",  img: img3
+    },
+    {
+      i: 4,
+      content: "IOS. Chọn mục Thêm bằng mã QR (Add using QR code)",  img: img4
+    },
+    {
+      i: 5,
+      content:
+        "IOS. Đưa camera hướng vào mã QR sao cho mã QR nằm trong đường hướng dẫn để quét",  img: img5
+    },
+  ];
+
+
+
+  export {guideSteps1, guideSteps2}
