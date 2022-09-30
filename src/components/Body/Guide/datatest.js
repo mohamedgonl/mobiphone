69ffeb4ccb2cfe0d485abe6879d9390c8af80807
Gonl <long.nq.hust@gmail.com>
2022-09-30 14:18:23 +0700

    done

--- src/components/Body/Guide/datatest.js
+++ src/components/Body/Guide/datatest.js
@@ -1,8 +1,4 @@
-import img1 from "../../../images/img1.jpg";
-import img2 from "../../../images/img2.jpg";
-import img3 from "../../../images/img3.jpg";
-import img4 from "../../../images/img4.jpg";
-import img5 from "../../../images/img5.jpg";
+
 import img6 from '../../../images/img6.jpg';
 import img7 from '../../../images/img7.jpg';
 const guideSteps1 = [
@@ -10,33 +6,33 @@ const guideSteps1 = [
     {
       i: 2,
       content:
-        "Android. Chọn quản lý Sim (Sim card manager) và chọn thêm gói di động (Add mobile plan)",  img: img2
+        "Android. Chọn quản lý Sim (Sim card manager) và chọn thêm gói di động (Add mobile plan)",  img: img7
     },
     {
       i: 3,
-      content: "Android. Chọn Thêm gói cước di động (Add mobile plan)",  img: img3
+      content: "Android. Chọn Thêm gói cước di động (Add mobile plan)",  img: img7
     },
     {
       i: 4,
-      content: "Android. Chọn mục Thêm bằng mã QR (Add using QR code)",  img: img4
+      content: "Android. Chọn mục Thêm bằng mã QR (Add using QR code)",  img: img6
     },
     {
       i: 5,
       content:
-        "Android. Đưa camera hướng vào mã QR sao cho mã QR nằm trong đường hướng dẫn để quét",  img: img5
+        "Android. Đưa camera hướng vào mã QR sao cho mã QR nằm trong đường hướng dẫn để quét",  img: img7
     },
     {
       i: 6,
-      content: "Android. Khi gói di động được quét ra, chạm vào Thêm (Add) để đăng ký",  img: img1
+      content: "Android. Khi gói di động được quét ra, chạm vào Thêm (Add) để đăng ký",  img: img6
     },
     {
       i: 7,
-      content: "Android. Chạm vào OK để bật gói di động",  img: img2
+      content: "Android. Chạm vào OK để bật gói di động",  img: img7
     },
     {
       i: 8,
       content:
-        "Android. Kết quả: eSIM sau khi được kích hoạt sẽ xuất hiện trong mục eSIM như hình",  img: img3
+        "Android. Kết quả: eSIM sau khi được kích hoạt sẽ xuất hiện trong mục eSIM như hình",  img: img6
     },
   ];
 
@@ -47,20 +43,20 @@ const guideSteps1 = [
     {
       i: 2,
       content:
-        "IOS. Chọn quản lý Sim (Sim card manager) và chọn thêm gói di động (Add mobile plan)",  img: img2
+        "IOS. Chọn quản lý Sim (Sim card manager) và chọn thêm gói di động (Add mobile plan)",  img: img6
     },
     {
       i: 3,
-      content: "IOS. Chọn Thêm gói cước di động (Add mobile plan)",  img: img3
+      content: "IOS. Chọn Thêm gói cước di động (Add mobile plan)",  img: img7
     },
     {
       i: 4,
-      content: "IOS. Chọn mục Thêm bằng mã QR (Add using QR code)",  img: img4
+      content: "IOS. Chọn mục Thêm bằng mã QR (Add using QR code)",  img: img6
     },
     {
       i: 5,
       content:
-        "IOS. Đưa camera hướng vào mã QR sao cho mã QR nằm trong đường hướng dẫn để quét",  img: img5
+        "IOS. Đưa camera hướng vào mã QR sao cho mã QR nằm trong đường hướng dẫn để quét",  img: img7
     },
   ];
 
