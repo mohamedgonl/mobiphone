41d6802d5f68b30beb9eacfec649fe65f06e50ad
Gonl <long.nq.hust@gmail.com>
2022-10-03 16:02:50 +0700

    Ghep API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@
 import './App.css';
 import Body from './components/Body';
 import Header from './components/NavBar';
+import {useState, useEffect} from 'react'
+const url = 'https://www.mobifone.vn/get-data-micro-esim';
 
 function App() {
+  const [data, setData] = useState();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(url,{
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',// 'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic bWlyY29lc2ltOkVzaW1AMjAyMiM='
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    })
+    .then((res) => {
+     if(res.ok) {
+       return res.json();
+      }
+      throw new Error('Server bad response')
+    })
+    .then(res => {
+      setLoaded(true)
+      setData(res.data)
+    })
+    .catch(err => console.log('Error::::', err))
+  }, []);
+
   return (
-    <>
+    loaded &&
+      <> 
       <Header></Header>
-      <Body></Body>
-    </>
+      <Body data = {data}></Body> 
+      </>
+
   );
 }
 
--- src/components/Body/Reasons/index.js
+++ src/components/Body/Reasons/index.js
@@ -1,62 +1,10 @@
 import "../style.css";
 import React from "react";
-import { useState, useEffect } from "react";
-import icon1 from "../../../images/icon1.svg";
-import icon2 from "../../../images/icon2.svg";
-import icon3 from "../../../images/icon3.svg";
+import { useState} from "react";
+import parse from 'html-react-parser';
 
-var reasons11 = [
-  {
-    icon: icon1,
-    title: "Sử dụng nhiều số điện thoại trên 1 thiết bị",
-    content: "Không cần lắp đặt nhiều sim vật lý trên điện thoại.",
-  },
-  {
-    icon: icon2,
-    title: "Tránh hỏng mất SIM",
-    content:
-      "eSIM có phần cứng được lắp vào thiết bị di động, phần mềm được cài đặt quản lý SIM được tải và cài đặt ngay trên máy.",
-  },
-  {
-    icon: icon3,
-    title: "Mạng di động Nhanh - Ổn định - Khắp mọi nơi",
-    content:
-      "Trải nghiệm eSIM Viettel với tốc độ mạng 3G/4G nhanh và ổn định, cùng dịch vụ chăm sóc khách hàng hoàn hảo 24/7.",
-  },
-  {
-    icon: icon1,
-    title: "Test1",
-    content: "Test1",
-  },
-  {
-    icon: icon1,
-    title: "Test2",
-    content: "Test2",
-  },
-  {
-    icon: icon1,
-    title: "Test3",
-    content: "Test3",
-  },
-  {
-    icon: icon1,
-    title: "Test4",
-    content: "Test4",
-  },
-];
-
-const Reasons = () => {
-  const [reasons, setReasons] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-
-  // CALL API
-  useEffect(() => {
-    setInterval(() => {
-      setReasons(reasons11);
-      setLoaded(true);
-    }, 3000);
-  }, [loaded]);
-
+const Reasons = ({data}) => {
+  const [reasons] = useState([...data]);
   const changeReasonsShowing = (i) => {
     let listReasons = document.querySelector(".body-reasons-list");
     listReasons.scrollBy({ left: i, top: 0, behavior: "smooth" });
@@ -69,15 +17,15 @@ const Reasons = () => {
           <img alt="icon" src={icon}></img>
         </div>
         <div className="reason-content">
-          <p className="reason-content-title">{title}</p>
-          <hr></hr>
-          <p className="reason-content-main">{content}</p>
+          <div className="reason-content-title">{parse(title)}</div>
+          {/* <hr></hr>
+          <p className="reason-content-main">{content}</p> */}
         </div>
       </div>
     );
   };
   return (
-    loaded && (
+     (
       <div className="body-reasons">
         <div className="body-reasons-header">
           <h1>Vì sao bạn cần sử dụng eSIM MobiFone?</h1>
@@ -90,8 +38,8 @@ const Reasons = () => {
           >
           </button>
           <div className="body-reasons-list">
-            {reasons.map((e,i) => (
-              <Reason title={e.title} content={e.content} icon={e.icon} />
+            {reasons.map((e) => (
+              <Reason title={e.description} content={e.content} icon={e.icon} />
             ))}
           </div>
           <button
